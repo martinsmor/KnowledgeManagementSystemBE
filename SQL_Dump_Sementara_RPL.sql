3e154cfc0cd73ef4c5f0cd63989179538517bedd
Rayhan Zaki Abrar <222011320@stis.ac.id>
2022-11-13 18:42:04 +0700

    update database (delete tags,media,kategori;update content;add feedback

--- SQL_Dump_Sementara_RPL.sql
+++ SQL_Dump_Sementara_RPL.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 07, 2022 at 07:40 AM
+-- Generation Time: Nov 13, 2022 at 12:39 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.5
 
@@ -34,7 +34,10 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`id`, `contentId`, `username`, `isi_comment`, `tanggal`) VALUES
-(1, 2, 'user1', 'Waduh sia sia dong panen ganja 1 ton', '2022-11-16');
+(1, 2, 'user1', 'Waduh sia sia dong panen ganja 1 ton', '2022-11-16'),
+(2, 1, 'user4', 'Kamu nanya?', '0000-00-00'),
+(3, 1, 'user4', 'Kamu nanya?', '0000-00-00'),
+(4, 5, 'user4', 'Kamu nanya?', '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -48,9 +51,10 @@ CREATE TABLE `content` (
   `tanggal` date NOT NULL,
   `judul` varchar(50) NOT NULL,
   `isi_konten` longtext NOT NULL,
+  `thumbnail` varchar(50) DEFAULT NULL,
   `liked` int(11) NOT NULL,
-  `type` varchar(50) NOT NULL,
   `kategori` varchar(50) NOT NULL,
+  `tags` text NOT NULL,
   `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -58,35 +62,36 @@ CREATE TABLE `content` (
 -- Dumping data for table `content`
 --
 
-INSERT INTO `content` (`contentId`, `username`, `tanggal`, `judul`, `isi_konten`, `liked`, `type`, `kategori`, `status`) VALUES
-(1, 'user3', '2022-11-10', 'Pedoman Survey untuk Inflasi', 'Inflasi:\r\n\r\nInflasi adalah kecenderungan naiknya harga barang dan jasa pada umumnya yang berlangsung secara terus menerus. Jika harga barang dan jasa di dalam negeri meningkat, maka inflasi mengalami kenaikan. Naiknya harga barang dan jasa tersebut menyebabkan turunnya nilai uang. Dengan demikian, inflasi dapat juga diartikan sebagai penurunan nilai uang terhadap nilai barang dan jasa secara umum.\r\n\r\nIndeks Harga konsumen (IHK):\r\n\r\nIndeks yang menghitung rata-rata perubahan harga dari suatu paket barang dan jasa yang dikonsumsi oleh rumah tangga dalam kurun waktu tertentu. IHK merupakan indikator yang digunakan untuk mengukur tingkat inflasi. Perubahan IHK dari waktu ke waktu menggambarkan tingkat kenaikan (inflasi) atau tingkat penurunan (deflasi) dari barang dan jasa.\r\n\r\nAndil Inflasi:\r\n\r\nAndil Inflasi adalah besarnya sumbangan setiap komoditas yang mengalami fluktuasi harga terhadap inflasi atau deflasi yang terjadi di suatu kota atau secara nasional. Besarnya nilai perubahan indeks (inflasi/deflasi) yang terjadi setiap bulan, merupakan gabungan andil dari jenis barang/jasa yang mengalami fluktuasi harga pada bulan yang bersangkutan.', 0, 'Artikel', 'Pertanian', 'Menunggu'),
-(2, 'user5', '2022-11-13', 'Apakah bertani itu boleh?', 'Bertani itu boleh asalkan jangan ganja', 0, 'Pertanyaan', 'Ekonomi', 'Disetujui'),
-(3, 'user3', '2022-11-06', 'Tata Cara Pernikahan Hewan', 'Menurut dr Roslan Yusni Hasan, spesialis bedah saraf, manusia tidak mungkin bisa menghamili binatang. \"Kalau manusia yang bersetubuh dengan binatang memang banyak, tetapi tidak mungkin bisa hamil karena genusnya berbeda,\" kata dokter yang akrab disapa dr Ryu ini saat dihubungi Kompas.com, Senin (21/12/2015)', 2, 'Video', 'Pendidikan', 'Ditolak'),
-(4, 'user6', '2022-11-06', 'Lagu Apuse Mempengaruhi Peradaban', 'Apuse kokon dao Yarabe soren doreri Wuf lenso bani nema baki pase Apuse kokon dao Yarabe soren doreri Wuf lenso bani nema baki pase Arafabye aswarakwar Arafabye aswarakwar\r\n\r\nArtikel ini telah tayang di www.inews.id dengan judul \" Lirik Lagu Apuse Beserta Notnya, Arti dan Asal Daerahnya \", Klik untuk baca: https://www.inews.id/news/nasional/lirik-lagu-apuse-beserta-notnya-arti-dan-asal-daerahnya.\r\n\r\n\r\nDownload aplikasi Inews.id untuk akses berita lebih mudah dan cepat:\r\nhttps://www.inews.id/apps', 0, 'Audio', 'Sosial', 'Menunggu'),
-(5, 'user5', '2022-11-01', 'Koruptor', 'Korupsi adalah semua yang memiliki keterkaitan terhadap tindakan yang diancam dengan sanksi sebagaimana diatur didalam Undang-undang No. 31 Tahun 1999 Tentang Pemberantasan Tindak Pidana Korupsi dan Undang-undang No. 20 Tahun 2001 tentang pengubahan Atas UU No. 31 Tahun 1999 tentang Pemberantasan Tindak Pidana Korupsi[1][2]. Dan Peraturan Mahkamah Agung Nomor 1 tahun 2020[3].\r\n\r\nRasuah atau mencuri (bahasa Latin: corruptio dari kata kerja corrumpere yang bermakna busuk, haram, menggoyahkan, memutarbalik, menyogok, mencuri, maling) ialah tindakan pejabat publik, baik politisi maupun pegawai negeri sipil, serta pihak lain yang terlibat dalam tindakan itu yang secara tidak wajar dan tidak legal menyalahgunakan kepercayaan publik dan masyarakat yang dikuasakan kepada mereka untuk mendapatkan keuntungan sepihak.[4]', 0, 'Artikel', 'Inflasi', 'Disetujui'),
-(7, 'user3', '2022-11-02', 'Penurunan Kemiskinan', 'qwertyuiopasdfghjklzxcvbnm', 0, 'Artikel', 'Demografi', 'Menunggu');
+INSERT INTO `content` (`contentId`, `username`, `tanggal`, `judul`, `isi_konten`, `thumbnail`, `liked`, `kategori`, `tags`, `status`) VALUES
+(1, 'user3', '2022-11-13', 'Content Error', 'Kesalahan yang disebabkan oleh jawaban responden maupun kesalahan pencatatan oleh petugas. Pengukuran dilakukan dengan mengukur perbedaan isian yang tercantum pada dokumen hasil pencacahan GB SE dengan UC PES.', 'https://dmm0a91a1r04e.cloudfront.net/i3O7AswS3pMet', 0, 'Ekonomi', 'Survei Pencacahan 2022', 'Pending'),
+(2, 'user5', '2022-11-13', 'Pernikahan ini', 'testestes', NULL, 0, 'Kependudukan', '', 'Menunggu'),
+(3, 'user3', '2022-11-13', 'Pernikahan ini', 'ahhahahahaha', NULL, 2, 'Kependudukan', '', 'Menunggu'),
+(4, 'user6', '2022-11-13', 'Pernikahan ini', 'testestes', NULL, 0, 'Kependudukan', '', 'Approved'),
+(5, 'user5', '2022-11-13', 'Pernikahan ini', 'testestes', NULL, 0, 'Kependudukan', '', 'Menunggu'),
+(7, 'user3', '2022-11-13', 'Pernikahan ini', 'testestes', NULL, 0, 'Kependudukan', '', 'Menunggu'),
+(10, 'user1', '2022-11-13', 'Pernikahan Not Found', 'ahhahahahaha', 'https://upload.wikimedia.org/wikipedia/commons/3/3', 0, 'Kependudukan', 'SP2020 Rudi', 'Pending'),
+(15, 'user2', '2022-11-13', 'Pernikahan Not Found', 'ahhahahahaha', 'https://upload.wikimedia.org/wikipedia/commons/3/3', 0, 'Kependudukan', 'SP2020 Rudi', 'Pending'),
+(16, 'user2', '2022-11-13', 'Pernikahan Not Found', 'ahhahahahaha', 'https://upload.wikimedia.org/wikipedia/commons/3/3', 0, 'Kependudukan', 'SP2020 Rudi', 'Pending');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `c_type`
+-- Table structure for table `feedback`
 --
 
-CREATE TABLE `c_type` (
+CREATE TABLE `feedback` (
   `id` int(11) NOT NULL,
-  `type` varchar(50) NOT NULL
+  `contentId` int(50) NOT NULL,
+  `feedback` text NOT NULL,
+  `from` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `c_type`
+-- Dumping data for table `feedback`
 --
 
-INSERT INTO `c_type` (`id`, `type`) VALUES
-(1, 'Artikel'),
-(5, 'Audio'),
-(3, 'Foto'),
-(2, 'Pertanyaan'),
-(4, 'Video');
+INSERT INTO `feedback` (`id`, `contentId`, `feedback`, `from`) VALUES
+(1, 1, 'Kontennya busuk', 'user4');
 
 -- --------------------------------------------------------
 
@@ -108,9 +113,10 @@ INSERT INTO `kategori_list` (`kategoriId`, `nama_kategori`) VALUES
 (3, 'Ekonomi'),
 (6, 'Inflasi'),
 (2, 'Kependudukan'),
+(8, 'Kesejahteraan'),
 (4, 'Pendidikan'),
 (1, 'Pertanian'),
-(5, 'Sosial');
+(5, 'Zakat');
 
 -- --------------------------------------------------------
 
@@ -134,18 +140,6 @@ INSERT INTO `like` (`id`, `contentId`, `username`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `media`
---
-
-CREATE TABLE `media` (
-  `contentId` int(10) NOT NULL,
-  `mediaId` int(10) NOT NULL,
-  `medialink` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `role`
 --
 
@@ -161,34 +155,8 @@ CREATE TABLE `role` (
 INSERT INTO `role` (`id`, `role`) VALUES
 (1, 'Administrator'),
 (2, 'Approval'),
-(3, 'Content Creator');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tag`
---
-
-CREATE TABLE `tag` (
-  `id` int(11) NOT NULL,
-  `contentId` int(10) NOT NULL,
-  `tag` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tag`
---
-
-INSERT INTO `tag` (`id`, `contentId`, `tag`) VALUES
-(1, 1, 'Penjahat'),
-(2, 2, 'SP2020'),
-(3, 3, 'IndonesiaHebat'),
-(4, 4, 'KamiMaju'),
-(5, 5, 'DataBerkualitas'),
-(6, 4, 'MelawanKoruptor'),
-(7, 1, 'AyoBergerak'),
-(8, 2, 'KerjaKerjaKerja'),
-(9, 3, 'PantangMenyerah');
+(3, 'Content Creator'),
+(4, 'Mitra');
 
 -- --------------------------------------------------------
 
@@ -207,10 +175,10 @@ CREATE TABLE `unit_kerja` (
 --
 
 INSERT INTO `unit_kerja` (`id`, `unit_kerja_kode`, `unit_kerja`) VALUES
-(1, '1', 'INSIS'),
 (2, '2', 'VISDAT'),
 (3, '3', 'METSTAT'),
-(4, '4', 'SAMPLING');
+(4, '4', 'SAMPLING'),
+(6, '6', 'SPD');
 
 -- --------------------------------------------------------
 
@@ -233,7 +201,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`username`, `password`, `nama`, `role`, `unit_kerja`) VALUES
 ('user1', '$2y$10$WRXO6PXDyXo8ZChh61RqJOB269EReckrYgIcG5odKP2ps7RRKUruW', 'Binog', 'Administrator', 'INSIS'),
 ('user2', '$2y$10$vOuTNHPywPU2Pnns9G6WLOxsKMvXpMZFLflfld27c7L2MDbfk/KvW', 'Nakano Miku', 'Approval', 'METSTAT'),
-('user3', '$2y$10$bV01VA1.riWmxCuVeFvKmOt/Cj3WtTqMz3Y7NzXD2juy/FNsZOzcq', 'Aizen-sama', 'Content Creator', 'METSTAT'),
+('user3', '$2y$10$bV01VA1.riWmxCuVeFvKmOt/Cj3WtTqMz3Y7NzXD2juy/FNsZOzcq', 'Aizen-sama', 'Mitra', 'METSTAT'),
 ('user4', '$2y$10$irZ87tbdZuUyNMQEyGv8CemzDfbhMB1FcNlP2pwZad1ij2JshOPAG', 'Gojo Satoru', 'Approval', 'SAMPLING'),
 ('user5', '$2y$10$SgfTSDydLz6.ANKSLf8mV./c7Pfemz30c6BqzBaKvYoRWj/t1tD7O', 'Kitagawa Marin', 'Content Creator', 'SAMPLING'),
 ('user6', '$2y$10$L6cELWnWqjWpb3TE30Pw5O2oYEe.06k0yCtrdSa7Ix5e2h3k6ZSC6', 'Katou Megumi', 'Content Creator', 'INSIS');
@@ -253,15 +221,13 @@ ALTER TABLE `comment`
 --
 ALTER TABLE `content`
   ADD PRIMARY KEY (`contentId`),
-  ADD KEY `type` (`type`),
   ADD KEY `kategori` (`kategori`);
 
 --
--- Indexes for table `c_type`
+-- Indexes for table `feedback`
 --
-ALTER TABLE `c_type`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `type` (`type`);
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `kategori_list`
@@ -278,25 +244,11 @@ ALTER TABLE `like`
   ADD KEY `cont_like` (`contentId`);
 
 --
--- Indexes for table `media`
---
-ALTER TABLE `media`
-  ADD PRIMARY KEY (`mediaId`),
-  ADD KEY `id_cont` (`contentId`);
-
---
 -- Indexes for table `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `role` (`role`);
-
---
--- Indexes for table `tag`
---
-ALTER TABLE `tag`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `content` (`contentId`);
 
 --
 -- Indexes for table `unit_kerja`
@@ -320,25 +272,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `content`
 --
 ALTER TABLE `content`
-  MODIFY `contentId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `contentId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
--- AUTO_INCREMENT for table `c_type`
+-- AUTO_INCREMENT for table `feedback`
 --
-ALTER TABLE `c_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `feedback`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `kategori_list`
 --
 ALTER TABLE `kategori_list`
-  MODIFY `kategoriId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `kategoriId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `like`
@@ -347,28 +299,16 @@ ALTER TABLE `like`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `media`
---
-ALTER TABLE `media`
-  MODIFY `mediaId` int(10) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `tag`
---
-ALTER TABLE `tag`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `unit_kerja`
 --
 ALTER TABLE `unit_kerja`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
@@ -378,24 +318,11 @@ ALTER TABLE `unit_kerja`
 -- Constraints for table `content`
 --
 ALTER TABLE `content`
-  ADD CONSTRAINT `kategori` FOREIGN KEY (`kategori`) REFERENCES `kategori_list` (`nama_kategori`),
-  ADD CONSTRAINT `type` FOREIGN KEY (`type`) REFERENCES `c_type` (`type`);
+  ADD CONSTRAINT `kategori` FOREIGN KEY (`kategori`) REFERENCES `kategori_list` (`nama_kategori`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `like`
 --
 ALTER TABLE `like`
   ADD CONSTRAINT `cont_like` FOREIGN KEY (`contentId`) REFERENCES `content` (`contentId`);
-
---
--- Constraints for table `media`
---
-ALTER TABLE `media`
-  ADD CONSTRAINT `id_cont` FOREIGN KEY (`contentId`) REFERENCES `content` (`contentId`);
-
---
--- Constraints for table `tag`
---
-ALTER TABLE `tag`
-  ADD CONSTRAINT `content` FOREIGN KEY (`contentId`) REFERENCES `content` (`contentId`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
