2e73a2ba05497385b42e3c6e05589294801b288a
Rayhan Zaki Abrar <222011320@stis.ac.id>
2022-11-14 17:00:38 +0700

    change get to post for login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -37,7 +37,7 @@ $routes->set404Override();
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->post('register', 'Register::index');
-$routes->get('api/auth', 'Login::index');
+$routes->post('api/auth', 'Login::index');
 
 $routes->get('api/beranda','Beranda::index');
 
