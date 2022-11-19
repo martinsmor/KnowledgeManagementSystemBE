daed9bc086c47e3f30a4aa7175391140a906c514
Rayhan Zaki Abrar <222011320@stis.ac.id>
2022-11-19 15:43:03 +0700

    post like

---
+++ app/Controllers/Likes.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\LikeModel;
+use CodeIgniter\RESTful\ResourceController;
+
+class Likes extends ResourceController
+{
+    /**
+     * Return an array of resource objects, themselves in array format
+     *
+     * @return mixed
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Return the properties of a resource object
+     *
+     * @return mixed
+     */
+    public function show($id = null)
+    {
+        //
+    }
+
+    /**
+     * Return a new resource object, with default properties
+     *
+     * @return mixed
+     */
+    public function new()
+    {
+        //
+    }
+
+    /**
+     * Create a new resource object, from "posted" parameters
+     *
+     * @return mixed
+     */
+    public function create($id = null)
+    {
+        $model = new LikeModel();
+        $data = [
+            'contentId' => $id,
+            'username'  => $this->request->getVar('username')
+        ];
+        $model->insert($data);
+        $response = [
+            'status'   => 201,
+            'error'    => null,
+            'messages' => [
+                'success' => $data['username'] . ' berhasil melakukan like pada konten : '.$data['contentId']
+            ]
+        ];
+        return $this->respondCreated($response);
+    }
+
+    /**
+     * Return the editable properties of a resource object
+     *
+     * @return mixed
+     */
+    public function edit($id = null)
+    {
+        //
+    }
+
+    /**
+     * Add or update a model resource, from "posted" properties
+     *
+     * @return mixed
+     */
+    public function update($id = null)
+    {
+        //
+    }
+
+    /**
+     * Delete the designated resource object from the model
+     *
+     * @return mixed
+     */
+    public function delete($id = null)
+    {
+        $username = $this->request->getVar('username');
+        $model = new LikeModel();
+        $data = $model->where('contentId', $id)->first();
+        if ($data) {
+            $model->delete(['contentId',$id]);
+            $response = [
+                'status'   => 200,
+                'error'    => null,
+                'messages' => [
+                    'success' => 'Unlike berhasil',
+                    'contentId' => $id,
+                    'username' => $username
+                ]
+            ];
+            return $this->respondDeleted($response);
+        } else {
+            $response = [
+                'status'   => 404,
+                'error'    => null,
+                'messages' => [
+                    'success' => 'Gagal',
+                    'contentId' => $id,
+                    'username' => $username
+                ]
+            ];
+            return $this->respond($response);
+        }
+    }
+}
